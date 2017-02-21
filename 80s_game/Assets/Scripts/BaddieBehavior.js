f3cb56f2b55235bd3fecd686945803db54c8e3ea
melbandit <melissa.i.sattler@gmail.com>
2017-02-21 17:56:37 -0500

    Baddie update

--- 80s_game/Assets/Scripts/BaddieBehavior.js
+++ 80s_game/Assets/Scripts/BaddieBehavior.js
@@ -3,7 +3,10 @@
 public var leftLimiter:GameObject;
 public var rightLimiter:GameObject;
 private var sprite:SpriteRenderer;
+private var animController:Animator;
 //public var player:GameObject;
+
+public var isDead:boolean = false;
 
 public var speed:float = 0.02;
 
@@ -11,6 +14,7 @@ private var direction:int = 1; //1 right -1 left
 
 function Start () {
 	sprite = this.GetComponent(SpriteRenderer);
+	animController = this.GetComponent(Animator);
 }
 
 function Update () {
@@ -23,14 +27,17 @@ function Update () {
 //		direction = -1;
 //		sprite.flipX = true;
 //	}
-	this.transform.position.x += speed * direction;
 
-	if(transform.position.x < leftLimiter.transform.position.x){
-		direction = 1;
-		sprite.flipX = false;
-	} else if(transform.position.x > rightLimiter.transform.position.x){ 
-		direction = -1;
-		sprite.flipX = true;
+	if (!isDead) {
+		this.transform.position.x += speed * direction;
+
+		if(transform.position.x < leftLimiter.transform.position.x){
+			direction = 1;
+			sprite.flipX = false;
+		} else if(transform.position.x > rightLimiter.transform.position.x){ 
+			direction = -1;
+			sprite.flipX = true;
+		}
 	}
 }
 
@@ -38,8 +45,12 @@ function OnCollisionEnter2D(other:Collision2D){
 	if(other.gameObject.tag == "Player"){
 		var rb:Rigidbody2D = other.gameObject.GetComponent("Rigidbody2D");
 		Debug.Log(rb.velocity.y);
-		if (rb.velocity.y > 0.1) {
-			Debug.Log("player kill baddie!");
+		if (rb.velocity.y > 0.01) {
+			Debug.Log("player killed baddie!");
+			isDead = true;
+			animController.SetBool("death", true);
+		} else {
+//			animController.SetBool("death", false);
 		}
 	}
 }
