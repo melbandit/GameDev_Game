16811df61d7285f1a374174fc776c48482f92e64
melbandit <melissa.i.sattler@gmail.com>
2017-02-25 18:51:40 -0500

    Adding level two portal

---
+++ 80s_game/Assets/Scripts/ToNextLevel2.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+
+function OnTriggerEnter2D(other:Collider){
+
+	if(other.gameObject.tag == "Player"){
+
+		Application.LoadLevel("ToNextLevel2");
+	}
+}
