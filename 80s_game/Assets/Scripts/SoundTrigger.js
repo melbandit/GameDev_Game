f21ac2980aa210caaf4428654e374cd66a45540a
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2017-03-14 18:34:05 -0400

    Platforms go boing

---
+++ 80s_game/Assets/Scripts/SoundTrigger.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var player:GameObject;
+
+function OnTriggerEnter2D(player:Collider2D){
+	if(player.gameObject.tag == "Player"){
+	 Debug.Log("Boing!!");
+	 var audio: AudioSource = GetComponent.<AudioSource>();
+	 audio.Play();
+
+	}
+}
